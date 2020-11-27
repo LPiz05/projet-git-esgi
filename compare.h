8229fca79bd4ca0b679689a5479774b75012ac81
Luis <pereiravazluis@gmail.com>
2020-11-27 16:29:53 +0100

    Add all structures and prototype in compare.h

--- compare.h
+++ compare.h
@@ -6,5 +6,28 @@
 #define PROJET_GIT_ESGI_COMPARE_H
 
 
+struct Ids
+{
+    char **ids;
+    int size;
+} Ids;
+
+struct Ids *ids;
+
+int compare(struct Tag *, struct DtdTag *);
+
+int compareChilds(struct Tag *tag, struct DtdTag *dtdTag);
+
+int checkTagName(char *, char *, char *);
+
+int checkAttributs(struct Attr **, int size, struct DtdAttr **, int sizeDtdAttr);
+int checkAttrName(char *attrName, char *dtdAttrName);
+int checkAttrValue(char *value, struct DtdAttr *dtdAttr);
+int checkAttrValues(char *value, char **values, int valuesSize);
+int checkAttrType(char *value, char *type);
+int inId(char *value);
+void addId(char *value);
+
+int checkContent(char *content, char *contentType);
 
 #endif //PROJET_GIT_ESGI_COMPARE_H
