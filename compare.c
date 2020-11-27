fe4803cc0d569287671a12a8143e5a161725af2d
Luis <pereiravazluis@gmail.com>
2020-11-27 16:27:32 +0100

    Create compare.c and compare.h

---
+++ compare.c
@@ -0,0 +1,5 @@
+//
+// Created by luis on 27/11/2020.
+//
+
+#include "compare.h"
---
+++ compare.h
@@ -0,0 +1,10 @@
+//
+// Created by luis on 27/11/2020.
+//
+
+#ifndef PROJET_GIT_ESGI_COMPARE_H
+#define PROJET_GIT_ESGI_COMPARE_H
+
+
+
+#endif //PROJET_GIT_ESGI_COMPARE_H
